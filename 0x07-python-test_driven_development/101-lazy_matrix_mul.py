3d69715d83b233986e60d98dfd836b1a8e2677d2
Godslove Udo <udogodslove@gmail.com>
2021-10-05 11:28:56 -0700

    all

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Contains a lazy_matrix_mul function for a TDD project.
+'''
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    '''Multiplies 2 matrices.
+    Args:
+        m_a (list): The first matrix.
+        m_b (list): The second matrix.
+    Returns:
+        list: A list of lists of the products of the two given matrices.
+    '''
+    return np.matmul(m_a, m_b)
