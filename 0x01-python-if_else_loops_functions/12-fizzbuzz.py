a40aa17880f4de0fc9f92b2a2de87a449ffc21af
Godslove Udo <udogodslove@gmail.com>
2022-03-08 11:49:09 -0800

    all

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This prints the numbers from 1 to 100
+multiples of three print Fizz
+instead of the number and
+multiples of five print Buzz
+numbers which are multiples of both
+three and five print FizzBuzz
+"""
+
+
 def fizzbuzz():
     for i in range(1, 101):
         elif i % 3 == 0 and i % 5 == 0:
