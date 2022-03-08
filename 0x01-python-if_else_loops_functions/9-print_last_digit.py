65d03570b3e3c27e1859c93bff98f3815b7fce62
Godslove Udo <udogodslove@gmail.com>
2022-03-08 11:10:48 -0800

    all

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,3 @@ def print_last_digit(number):
     lastDigit = int(repr(number)[-1])
     print('{}'.format(lastDigit), end='')
     return lastDigit
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
